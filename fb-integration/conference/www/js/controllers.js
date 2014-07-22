317aefdf43ecfe71d154fd8b9f1c44133b436d03
sjbgit <sjb_strat@hotmail.com>
2014-07-22 18:24:52 -0500

    session controller

--- fb-integration/conference/www/js/controllers.js
+++ fb-integration/conference/www/js/controllers.js
@@ -33,16 +33,9 @@ angular.module('starter.controllers', ['starter.services'])
   };
 })
 
-.controller('PlaylistsCtrl', function($scope) {
-  $scope.playlists = [
-    { title: 'Reggae', id: 1 },
-    { title: 'Chill', id: 2 },
-    { title: 'Dubstep', id: 3 },
-    { title: 'Indie', id: 4 },
-    { title: 'Rap', id: 5 },
-    { title: 'Cowbell', id: 6 }
-  ];
-})
+    .controller('SessionsCtrl', function($scope, Session) {
+        $scope.sessions = Session.query();
+    })
 
 .controller('PlaylistCtrl', function($scope, $stateParams) {
 })
