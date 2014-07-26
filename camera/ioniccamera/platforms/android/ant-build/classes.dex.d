4d9e593b6fd6e95a5ce7a7d4494147dcc579bcd3
sjbgit <sjb_strat@hotmail.com>
2014-07-26 03:26:26 -0500

    popup

--- camera/ioniccamera/platforms/android/ant-build/classes.dex.d
+++ camera/ioniccamera/platforms/android/ant-build/classes.dex.d
@@ -9,5 +9,8 @@ C:\Users\Seth\Documents\GitHub\Ionic\camera\ioniccamera\platforms\android\ant-bu
 C:\Users\Seth\Documents\GitHub\Ionic\camera\ioniccamera\platforms\android\ant-build\classes\com\ionicframework\ioniccamera698407\R$string.class \
 C:\Users\Seth\Documents\GitHub\Ionic\camera\ioniccamera\platforms\android\ant-build\classes\com\ionicframework\ioniccamera698407\R$xml.class \
 C:\Users\Seth\Documents\GitHub\Ionic\camera\ioniccamera\platforms\android\ant-build\classes\com\ionicframework\ioniccamera698407\R.class \
+C:\Users\Seth\Documents\GitHub\Ionic\camera\ioniccamera\platforms\android\ant-build\classes\org\apache\cordova\camera\CameraLauncher.class \
+C:\Users\Seth\Documents\GitHub\Ionic\camera\ioniccamera\platforms\android\ant-build\classes\org\apache\cordova\camera\ExifHelper.class \
+C:\Users\Seth\Documents\GitHub\Ionic\camera\ioniccamera\platforms\android\ant-build\classes\org\apache\cordova\camera\FileHelper.class \
 C:\Users\Seth\Documents\GitHub\Ionic\camera\ioniccamera\platforms\android\ant-build\classes\org\apache\cordova\device\Device.class \
 C:\Users\Seth\Documents\GitHub\Ionic\camera\ioniccamera\platforms\android\ant-build\dexedLibs\classes-7eed370eac9a5687089a4ed944d3bd65.jar \
--- camera/ioniccamera/www/js/controllers.js
+++ camera/ioniccamera/www/js/controllers.js
@@ -1,6 +1,49 @@
 angular.module('starter.controllers', [])
 
-.controller('DashCtrl', function($scope) {
+.controller('DashCtrl', function($scope, $ionicPopup, $timeout) {
+
+        $scope.showAlert = function() {
+            var alertPopup = $ionicPopup.alert({
+                title: 'Don\'t eat that!',
+                template: 'It might taste good'
+            });
+            alertPopup.then(function(res) {
+                console.log('Thank you for not eating my delicious ice cream cone');
+            });
+        };
+
+        $scope.showPopup = function() {
+            $scope.data = {}
+
+            // An elaborate, custom popup
+            var myPopup = $ionicPopup.show({
+                template: '<input type="password" ng-model="data.wifi">',
+                title: 'Enter Wi-Fi Password',
+                subTitle: 'Please use normal things',
+                scope: $scope,
+                buttons: [
+                    { text: 'Cancel' },
+                    {
+                        text: '<b>Save</b>',
+                        type: 'button-positive',
+                        onTap: function(e) {
+                            if (!$scope.data.wifi) {
+                                //don't allow the user to close unless he enters wifi password
+                                e.preventDefault();
+                            } else {
+                                return $scope.data.wifi;
+                            }
+                        }
+                    },
+                ]
+            });
+            myPopup.then(function(res) {
+                console.log('Tapped!', res);
+            });
+            $timeout(function() {
+                myPopup.close(); //close the popup after 3 seconds for some reason
+            }, 3000);
+        };
 })
 
 .controller('FriendsCtrl', function($scope, Friends) {
