af1748c0962115ced161b6403af9b247ac9ea7d6
sjbgit <sjb_strat@hotmail.com>
2014-07-26 15:51:11 -0500

    camera

--- camera/ioniccamera/www/js/app.js
+++ camera/ioniccamera/www/js/app.js
@@ -4,8 +4,8 @@
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
 // 'starter.services' is found in services.js
-// 'starter.controllers' is found in controllers.js
-angular.module('starter', ['ionic', 'starter.controllers', 'starter.services'])
+// 'starter.controllers' is found in controllers.js   
+angular.module('starter', ['ionic', 'starter.controllers', 'starter.services', 'ngCordova'])
 
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
