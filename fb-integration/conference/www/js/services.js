60964e3cbabbd2051e872cf3814ae6c1d724c49a
sjbgit <sjb_strat@hotmail.com>
2014-07-25 23:38:53 -0500

    info

--- fb-integration/conference/www/js/services.js
+++ fb-integration/conference/www/js/services.js
@@ -2,7 +2,7 @@ angular.module('starter.services', ['ngResource'])
 
     //Session factory
 .factory('Session', function ($resource) {
-        //this should be configured
+        //this should be configured  
     return $resource('http://localhost:5000/sessions/:sessionId');
 });
 
