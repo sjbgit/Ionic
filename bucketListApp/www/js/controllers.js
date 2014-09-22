14b78bcd8d497681aba46aaff8c3d3d97e44b5bc
sjbgit <sjb_strat@hotmail.com>
2014-09-22 17:21:24 -0500

    update controllers

--- bucketListApp/www/js/controllers.js
+++ bucketListApp/www/js/controllers.js
@@ -1,3 +1,43 @@
 /**
  * Created by sbunke on 9/22/2014.
  */
+angular.module('bucketList.controllers', [])
+    .controller('SignUpCtrl', [
+        '$scope', '$rootScope', '$firebaseAuth', '$window',
+        function ($scope, $rootScope, $firebaseAuth, $window) {
+            $scope.user = {
+                email: "",
+                password: ""
+            };
+            $scope.createUser = function () {
+                var email = this.user.email;
+                var password = this.user.password;
+
+                if (!email || !password) {
+                    $rootScope.notify("Please enter valid credentials");
+                    return false;
+                }
+
+                $rootScope.show('Please wait.. Registering');
+                $rootScope.auth.$createUser(email, password, function (error, user) {
+                    if (!error) {
+                        $rootScope.hide();
+                        $rootScope.userEmail = user.email;
+                        $window.location.href = ('#/bucket/list');
+                    }
+                    else {
+                        $rootScope.hide();
+                        if (error.code == 'INVALID_EMAIL') {
+                            $rootScope.notify('Invalid Email Address');
+                        }
+                        else if (error.code == 'EMAIL_TAKEN') {
+                            $rootScope.notify('Email Address already taken');
+                        }
+                        else {
+                            $rootScope.notify('Oops something went wrong. Please try again later');
+                        }
+                    }
+                });
+            }
+        }
+    ])
