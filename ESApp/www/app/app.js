4d1341f3eac9a11cb0845aa510ea571a1dd1bea9
sjbgit <sjb_strat@hotmail.com>
2014-10-27 20:05:15 -0500

    added routing

--- ESApp/www/app/app.js
+++ ESApp/www/app/app.js
@@ -19,13 +19,13 @@ angular.module('eliteApp', ['ionic'])
 
     .state('home', {
     	abstract: true,
-      url: "/home",
-      //abstract: true,
-      templateUrl: "app/home/home.html" //,
-      //controller: 'AppCtrl'
+      	url: "/home",
+      	//abstract: true,
+      	templateUrl: "app/home/home.html" //,
+      	//controller: 'AppCtrl'
     })
 
-
+    //named views because have multiple views on home page
     .state('home.leagues', {
     	url: "/leagues",
     	views: {
@@ -45,14 +45,69 @@ angular.module('eliteApp', ['ionic'])
     })
 
     .state('app', {
+    	abstract: true,
     	url: "/app",
     	templateUrl: "app/layout/menu-layout.html"
 
     	})
 
+    .state('app.teams', {
+    	url: "/teams",
+    	views: {
+    		'mainContent': {
+    			templateUrl: "app/teams/teams.html"
+    		}
+    	}
+    })
+
+    .state('app.team-detail', {
+      url: "/teams/:id",
+      views: {
+        'mainContent': {
+          templateUrl: "app/teams/team-detail.html"
+        }
+      }
+    })
+
+    .state('app.game', {
+      url: "/game/:id",
+      views: {
+        'mainContent': {
+          templateUrl: "app/game/game.html"
+        }
+      }
+    })
+
+    .state('app.standings', {
+      url: "/standings",
+      views: {
+        'mainContent': {
+          templateUrl: "app/standings/standings.html"
+        }
+      }
+    })
+
+    .state('app.locations', {
+      url: "/locations",
+      views: {
+        'mainContent': {
+          templateUrl: "app/locations/locations.html"
+        }
+      }
+    })
+
+    .state('app.rules', {
+      url: "/rules",
+      views: {
+        'mainContent': {
+          templateUrl: "app/rules/rules.html",
+        }
+      }
+    })
+
     ;
     
     //});
   // if none of the above states are matched, use this as the fallback
-  $urlRouterProvider.otherwise('/home');
+  $urlRouterProvider.otherwise('/app/teams');
 });
