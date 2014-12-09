d93744f9ea5602ae6f6f08da7d3fdaa655007fc3
sjbgit <sjb_strat@hotmail.com>
2014-12-09 14:56:34 -0600

    more maps

--- ESApp/www/app/locations/test-location-ctrl.js
+++ ESApp/www/app/locations/test-location-ctrl.js
@@ -2,22 +2,45 @@
 
 	'use strict';
 
-	angular.module('eliteApp').controller('TestLocationCtrl', ['eliteApi', TestLocationCtrl]);
+	angular.module('eliteApp').controller('TestLocationCtrl', ['eliteApi', '$ionicLoading', TestLocationCtrl]);
 
-	function TestLocationCtrl(eliteApi) {
+	function TestLocationCtrl(eliteApi, $ionicLoading) {
 		var vm = this;
 
 		vm.info = 'test';
 
-		vm.map = { center: { latitude: 45, longitude: -73 }, zoom: 8 };
+		//vm.map = { center: { latitude: 45, longitude: -73 }, zoom: 8 };
 
-		/*
+		
 
 		vm.mapCreated = function(map) {
     		vm.map = map;
   		};
 
-		*/
+  		vm.centerOnMe = function () {
+    		console.log("Centering"); 
+    		if (!vm.map) {
+      			return;
+    		}
+
+    		/*
+    		vm.loading = $ionicLoading.show({
+      			content: 'Getting current location...',
+      			showBackdrop: false
+    		});
+			*/
+
+    		navigator.geolocation.getCurrentPosition(function (pos) {
+      			console.log('Got pos', pos);
+      			vm.map.setCenter(new google.maps.LatLng(pos.coords.latitude, pos.coords.longitude));
+      			vm.loading.hide();
+    		}, function (error) {
+      			alert('Unable to get location: ' + error.message);
+    		});
+  		};
+
+
+		
   
 	};
 
