85383a2232617455050be2cbfcc674747d4f6e8e
sjbgit <sjb_strat@hotmail.com>
2015-01-02 20:00:17 -0600

    map not working

--- ESApp/www/app/locations/location-map-ctrl.js
+++ ESApp/www/app/locations/location-map-ctrl.js
@@ -2,10 +2,10 @@
 
 	'use strict';
 
-	angular.module('eliteApp').controller('LocationMapCtrl', ['$stateParams', 'eliteApi', LocationMapCtrl]);
+	angular.module('eliteApp').controller('LocationMapCtrl', ['$stateParams', 'eliteApi', 'mapService', LocationMapCtrl]);
 
 
-	function LocationMapCtrl($stateParams, eliteApi) {
+	function LocationMapCtrl($stateParams, eliteApi, mapService) {
 		var vm = this;
 
 		vm.locationId = Number($stateParams.id); 
@@ -31,11 +31,35 @@
             };
 
             vm.map.center.latitude = vm.location.latitude;
-            vm.map.center.longitude = vm.location.longitude;
+            vm.map.center.longitude = vm.location.longitude; 
+
+          vm.mapOptions   = {
+          center: new google.maps.LatLng(vm.map.center.latitude, vm.map.center.longitude), //(41.881832, -87.623177), //center: new google.maps.LatLng(20.8861, 156.6747), //43.07493, -89.381388),
+          zoom: 16,
+          mapTypeId: google.maps.MapTypeId.ROADMAP
+        	};
+
+        	//data gets there too late
+        	mapService.mapOptions = vm.mapOptions;
         });
+
+/*
+        var mapOptions = {
+          center: new google.maps.LatLng(43.005416, -88.221413), //(41.881832, -87.623177), //center: new google.maps.LatLng(20.8861, 156.6747), //43.07493, -89.381388),
+          zoom: 16,
+          mapTypeId: google.maps.MapTypeId.ROADMAP
+        };
+*/
+
+    //vm.mapOptions = mapOptions;
+
+    //mapService.mapOptions = mapOptions;
+
+
 
 		
 
+        //vm.mapOptions = mapOptions;
 
 		/*
 		vm.map = {
